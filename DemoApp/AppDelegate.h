1a15acb037547467b99eca8a793ee24ee5c719aa
Gregory John Casamento <greg.casamento@gmail.com>
2022-07-06 09:39:10 -0400

    Initial Commit

---
+++ DemoApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DemoApp
+//
+//  Created by Gregory Casamento on 7/6/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
